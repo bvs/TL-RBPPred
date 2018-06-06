f0db9aa09727eba891f51953812b1cd018b9f163
B V Suresh <gandipalem@gmail.com>
2018-06-07 00:36:38 +0530

    Update combineFeatures.R

--- R/combineFeatures.R
+++ R/combineFeatures.R
@@ -1,12 +1,1 @@
-### THis function combines features extracted from feature116.exe  and seqCTD.R ###
-combineFeatures <- function(seq,csv) {
-seq <- readFASTA(seq)
-ctd <- seqCTD(seq)
-seqCSV <- read.csv(csv,header=FALSE)
-seqCSV <- t(seqCSV[,-1])
-mat <- rbind(ctd,seqCSV)
-colnames(mat) <- names(seq)
-rownames(mat) <- NULL
-return(mat)
-}
-### End of combineFeatures ###
+
